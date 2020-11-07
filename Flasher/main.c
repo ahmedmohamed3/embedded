3ff60cc5e60bef014f6e761e9c56b6f045bca83b
ahmedmohamed3 <anaelmoaksss@gmail.com>
2020-11-07 19:05:58 +0200

    [modify-code]

--- Flasher/main.c
+++ Flasher/main.c
@@ -1,1 +1,38 @@
 #include <reg52.h>
+
+/*
+*Author: ahmed mohamed
+*Desc: output 5 Volt at port 3 pin 0
+*/
+
+//initializing the functions 
+void init();
+void run();
+
+//defining the global variables 
+sbit LED= P3^0;
+sbit BUTTON= P2^0;
+
+void main(void){
+	//initilize 
+	void init();
+	//do the logic 
+	void run();
+}
+void init(){
+	//the led is output and botton is input
+	LED=0;
+	BUTTON=1;
+}
+void run(){
+	while(1){
+		if(BUTTON==1){
+			//reads the groung
+			LED=1;
+		}else{
+			LED=0;
+		}
+	}
+}
+
+
